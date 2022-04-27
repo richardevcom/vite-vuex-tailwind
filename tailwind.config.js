85c4117a1f2d0f2fe43a682396965a357268b167
richardev <hello@richardev.com>
2022-04-27 19:25:40 +0300

    ✅ updated packages to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
-	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	media: false,
 	theme: {
 		extend: {},
 	},
